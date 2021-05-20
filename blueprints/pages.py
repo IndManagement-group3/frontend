39de724ef180280fdd8c9459df4bd4b3a91b44c7
Kirs67 <kirilel200267@gmail.com>
2021-05-20 20:48:21 +0300

    Frontend can be called "done" (for worst case)

--- blueprints/pages.py
+++ blueprints/pages.py
@@ -7,11 +7,7 @@ pages = Blueprint('pages', __name__, url_prefix="/")
 @pages.route("/")
 def mainpage():
 	try:
-		devices = [{"id": "new", "name": "New"}, {"id": "4fbd", "name": "old", "password": "password"}]
-
-		resp = make_response(render_template('mainpage.html'))
-		resp.set_cookie('devices', json.dumps(devices))
-		return resp
+		return render_template('mainpage.html')
 	except TemplateNotFound:
 		abort(404)
 
@@ -44,8 +40,6 @@ def devices():
 
 @pages.route("/add_device", methods = ['POST'])
 def add_device():
-	print("AHAHAHAHA")
-
 	if request.method == 'POST':
 
 		devices = json.loads(request.cookies.get('devices'))
@@ -54,9 +48,38 @@ def add_device():
 		name = request.form['name']
 		password = request.form['password']
 
+		#if some field is empty - redirect them back to the form
+		if id == '' or name == '' or password == '':
+			return redirect("/devices?id=new", code=302)
+
+		#if the device already exists - delete it first (thus updating it)
+		try:
+			dev = next(filter(lambda dev: dev.get('id') == id, devices))
+			devices.remove(dev)
+		except StopIteration:
+			pass
+
 		devices.append({"id": id, "name": name, "password": password})
 		
 		resp = make_response(redirect("/devices", code=302))
 		resp.set_cookie('devices', json.dumps(devices))
 
 		return resp
+
+@pages.route("/remove_device")
+def remove_device():
+	
+	devices = json.loads(request.cookies.get('devices'))
+	id = request.args.get("id")
+
+	try:
+		dev = next(filter(lambda dev: dev.get('id') == id, devices))
+	except StopIteration:
+		return redirect("/devices", code=302)
+
+	devices.remove(dev)
+		
+	resp = make_response(redirect("/devices", code=302))
+	resp.set_cookie('devices', json.dumps(devices))
+
+	return resp
